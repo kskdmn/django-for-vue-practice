7ae25b165a84466355b302e53c5df9e6345c572a
Keisuke Daimon <kdaimon.8@gmail.com>
2025-06-08 19:31:12 +0800

    Add an admin page for Sample

--- myproject/api/sample/admin.py
+++ myproject/api/sample/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class SampleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'created_by', 'created_at', 'updated_by', 'updated_at')
+    search_fields = ('name', 'description')
+    ordering = ('id',)
+
+admin.site.register(Sample, SampleAdmin)
